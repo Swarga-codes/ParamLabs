6c3cb1ae673d1bbe49241ded28461ca3e0f9b9e9
Swarga-codes <sd3526@srmist.edu.in>
2022-02-28 19:26:45 +0530

    iOS fixed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Landing from './components/Landing';
 import './App.css';
 import ParamVid from './assets/Paramlabs.mp4'
 import Navbar from './components/Navbar';
+
 function App() {
+  
+ 
   return (
     <div className="Background">
     <Navbar/> 
     <Landing/>
-    <video loop autoPlay muted>
+    <video loop autoPlay muted playsInline>
       <source
         src={ParamVid}
         type="video/mp4"
