c9cfce50b9b083b6ef7531c769c99efe2a54eb48
SwargaRajDutta <72154312+Swarga-codes@users.noreply.github.com>
2022-02-28 22:42:44 +0530

    Fixed

--- src/App.js
+++ src/App.js
@@ -5,31 +5,19 @@ import ParamVid from './assets/Paramlabs.mp4'
 import Navbar from './components/Navbar';
 import ParamVidLow from './assets/Paramlabs-low-compressed.mp4'
 function App() {
-  const [speed, setSpeed] = React.useState(navigator.connection.effectiveType);
-   
-  console.log(speed);
-
+  
   return (
     <div className="Background">
     <Navbar/> 
     <Landing/>
-    {speed === '4g'?   <video loop autoPlay muted playsInline>
+     <video loop autoPlay muted playsInline>
       <source
         src={ParamVid}
         type="video/mp4"
       />
       Your browser does not support the video tag
     </video>
-    :
     
-      <video loop autoPlay muted playsInline>
-      <source
-        src={ParamVidLow}
-        type="video/mp4"
-      />
-      Your browser does not support the video tag
-    </video>
-    }
    
   </div>
   );
